26e02583b27660ca309c94eda8df6a9ab5144061
Harshalvk <harshalvkhobragade@gmail.com>
2025-01-27 21:01:00 +0530

    add: simple express server
    Sends todos on / route
    Response 'Server running msg' on /success route

---
+++ src/index.ts
@@ -0,0 +1,40 @@
+import express from "express";
+
+const app = express();
+const PORT = 3000;
+
+const todos = [
+  {
+    id: 1,
+    task: "workout",
+    completed: false,
+    priority: "medium",
+    dueDate: "2023-10-15",
+  },
+  {
+    id: 2,
+    task: "study dev",
+    completed: false,
+    priority: "high",
+    dueDate: "2023-10-10",
+  },
+  {
+    id: 3,
+    task: "call at home",
+    completed: true,
+    priority: "low",
+    dueDate: "2023-10-05",
+  },
+];
+
+app.get("/success", (req, res) => {
+  res.send("Server is running");
+});
+
+app.get("/", (req, res) => {
+  res.json({ todos });
+});
+
+app.listen(PORT, () => {
+  console.log("⚙ Server is listening!");
+});
